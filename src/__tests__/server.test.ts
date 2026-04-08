1eac7d0cffe7aa97d1de4c203fbb701758fb5cef
Orveo <orveo1902@gmail.com>
2026-04-08 12:27:37 -0400

    Se realizo una ultima prueba para el catch del server

--- src/__tests__/server.test.ts
+++ src/__tests__/server.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest'
-import server from '../server'
+import server, {connectDB} from '../server'
+import db from '../config/db'
 
 describe('GET /api', () => {
     it('should send back a json response', async () => {
@@ -10,5 +11,19 @@ describe('GET /api', () => {
 
         expect(res.status).not.toBe(404)
         expect(res.body.msg).not.toBe('desde api')
+    })
+})
+
+//Probando la conexión de la base de datos. 
+jest.mock('../config/db')
+
+describe('Connect db', () => {
+    it('Should handle database connection error', async () => {
+        jest.spyOn(db,'authenticate').mockRejectedValueOnce(new Error('Hubo un error de conexión en la base de datos.')) //Se crea un espia, que va a esperar a que se ejecute el db.authenticate
+        const consoleSpy = jest.spyOn(console,'log') //Este espia va a revisar el console.log que se va a ejecutar en el catch 
+        await connectDB()
+        expect(consoleSpy).toHaveBeenCalledWith(
+            expect.stringContaining('Hubo un error de conexión en la base de datos.')
+        )
     })
 })
